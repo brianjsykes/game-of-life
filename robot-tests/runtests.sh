2d424d85f2491ec8f2e4849790001a8a44ddba71
Brian Sykes <bsykes.dev@gmail.com>
2016-02-29 02:06:11 -0800

    Update runtests.sh

--- robot-tests/runtests.sh
+++ robot-tests/runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 HOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
-cd /opt/robot
+cd /tmp/robot
 TEST_RESULT=$(xvfb-run pybot --variable URL:http://${HOST_IP}:9090 ./webtests.robot)
 echo $TEST_RESULT
 if [[ $(echo $TEST_RESULT | grep "| PASS |") ]]; then
